db9f0859283d7f5e12c5e436403176c084eb17fe
Greg <19007919@vcconnect.co.za>
2020-10-28 22:45:44 +0200

    "implemented firestore"

--- app/src/main/java/com/greg19007919/worqoutapp/Register.java
+++ app/src/main/java/com/greg19007919/worqoutapp/Register.java
@@ -7,35 +7,63 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import org.w3c.dom.Document;
 import org.w3c.dom.Text;
 
-public class Register extends AppCompatActivity {
-    EditText fullNameField, passwordField,emailField;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Register extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    public static final String TAG = "TAG";
+    EditText fullNameField, passwordField,emailField,weightField,heightField,ageField;
     Button registerButton;
     TextView toLoginButton;
     FirebaseAuth fAuth;
+    String userID;
+    FirebaseFirestore fStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ageField = findViewById(R.id.ageField);
+        weightField = findViewById(R.id.weightField);
+        heightField = findViewById(R.id.heightField);
         fullNameField = findViewById(R.id.fullNameField);
         passwordField = findViewById(R.id.passwordField);
         emailField = findViewById(R.id.emailField);
         registerButton = findViewById(R.id.registerButton);
         toLoginButton = findViewById(R.id.toLoginButton);
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+
+
+        final Spinner sexField = findViewById(R.id.sexField);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.sexSelect, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sexField.setAdapter(adapter);
+        sexField.setOnItemSelectedListener(this);
         if(fAuth.getCurrentUser() != null){
             startActivity(new Intent(getApplicationContext(),MainActivity.class));
             finish();
@@ -51,8 +79,13 @@ public class Register extends AppCompatActivity {
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = emailField.getText().toString().trim();
+                final String email = emailField.getText().toString().trim();
                 String password = passwordField.getText().toString().trim();
+                final String fullName = fullNameField.getText().toString();
+                final String height = heightField.getText().toString();
+                final String weight = weightField.getText().toString();
+                final String age = ageField.getText().toString();
+                final String sex = sexField.getSelectedItem().toString();
                 if(TextUtils.isEmpty(email)){
                     passwordField.setError("please enter an email address");
                 return;
@@ -62,8 +95,8 @@ public class Register extends AppCompatActivity {
                     return;
                 }
 
-                if(password.length() < 6){
-                    passwordField.setError("Please make sure your password is 6 characters or longer");
+                if(password.length() < 8){
+                    passwordField.setError("Please make sure your password is 8 characters or longer");
                     return;
                 }
 
@@ -72,6 +105,28 @@ public class Register extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(Register.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                            final String userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fStore.collection("users").document(userID);
+                            Map<String,Object> user = new HashMap<>();
+                            user.put("fullName",fullName);
+                            user.put("email",email);
+                            user.put("weight",weight);
+                            user.put("height",height);
+                            user.put("age",age);
+                            user.put("sex",sex);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "onSuccess: Profile created for" + userID);
+
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG,"OnFailure: " + e.toString());
+                                }
+                            });
+
                             startActivity(new Intent(getApplicationContext(),MainActivity.class));
                         }else{
                             Toast.makeText(Register.this, "Error:" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
@@ -82,5 +137,16 @@ public class Register extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
