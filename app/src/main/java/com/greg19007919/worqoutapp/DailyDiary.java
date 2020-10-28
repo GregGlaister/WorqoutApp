2f62afb5ea000dde96035382fab6a2460312ad33
Greg <19007919@vcconnect.co.za>
2020-10-28 23:54:20 +0200

    "12:00 hand-in Push"

---
+++ app/src/main/java/com/greg19007919/worqoutapp/DailyDiary.java
@@ -0,0 +1,23 @@
+package com.greg19007919.worqoutapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class DailyDiary extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+                
+            }
+        }
+
--- app/src/main/java/com/greg19007919/worqoutapp/MainActivity.java
+++ app/src/main/java/com/greg19007919/worqoutapp/MainActivity.java
@@ -11,12 +11,28 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     Button logOutButton;
-
+    Button diaryButton;
+    Button userInfoButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         logOutButton = findViewById(R.id.logOutButton);
+        diaryButton = findViewById(R.id.diaryButton);
+        userInfoButton = findViewById(R.id.userInfoButton);
+
+        diaryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),DailyDiary.class));
+            }
+        });
+        userInfoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),userInfo.class));
+            }
+        });
 
         logOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
